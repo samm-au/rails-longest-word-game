931f8505bd01122348743349b369b000ce02d259
Samantha Au <samanthaau@live.com>
2022-02-21 16:54:12 +1100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  TGE  /new(.:format)   games#new
+  POST /score(.:format) games#score
 end
